bff29b45d9c384f495b4dec7bdccfac7225d3a7d
vschool4566 <149108702+vschool4566@users.noreply.github.com>
2024-07-29 19:09:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const SignUp = require('./models.js');
-const cors = require('cors')
+
 const app = express();
-app.use(express.json());
-app.use(cors())
-mongoose.connect("mongodb+srv://service:service@cluster0.dpvyejs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+
+// Middleware
+app.use(express.json());  // To parse JSON bodies
+app.use(cors());          // To handle CORS
+
+// Connect to MongoDB Atlas
+mongoose.connect("mongodb+srv://service:service@cluster0.dpvyejs.mongodb.net/<database>?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log("Db connected...."))
-.catch(err => console.error("Db connection error:", err));
+.then(() => console.log("DB connected...."))
+.catch(err => console.error("DB connection error:", err));
 
+// Routes
 app.get('/hello', (req, res) => {
-    res.send("Hellosss");
+    res.send("Hello World");
 });
 
 app.post('/signup', async (req, res) => {
@@ -41,6 +47,7 @@ app.post('/signup', async (req, res) => {
     }
 });
 
+// Start server
 app.listen(8000, () => {
-    console.log("Running.....");
+    console.log("Server is running on port 8000");
 });
